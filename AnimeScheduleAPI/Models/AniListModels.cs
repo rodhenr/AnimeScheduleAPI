30ea9d8979d823964e69346dfde1be2a99914094
Rodrigo <rodhenrc@outlook.com>
2024-02-26 21:52:57 -0300

    feat: New service added, new models added

---
+++ AnimeScheduleAPI/Models/AniListModels.cs
@@ -0,0 +1,84 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AnimeScheduleAPI.Models;
+
+public record ApiResponse
+{
+    [JsonPropertyName("data")] 
+    public Data? Data { get; init; }
+}
+
+public record Data
+{
+    [JsonPropertyName("Page")] 
+    public Page? Page { get; set; }
+}
+
+public record Page
+{
+    [JsonPropertyName("media")] 
+    public IEnumerable<Anime> Media { get; init; }
+}
+
+public record Anime
+{
+    [JsonPropertyName("id")] 
+    public int Id { get; set; }
+
+    [JsonPropertyName("title")] 
+    public Title Titles { get; set; }
+
+    [JsonPropertyName("coverImage")] 
+    public CoverImage CoverImage { get; set; }
+
+    [JsonPropertyName("episodes")] 
+    public int? Episodes { get; set; }
+
+    [JsonPropertyName("siteUrl")] 
+    public string SiteUrl { get; set; } = null!;
+
+    [JsonPropertyName("externalLinks")] 
+    public IEnumerable<ExternalLink> ExternalLinks { get; set; }
+
+    [JsonPropertyName("airingSchedule")] 
+    public AiringSchedule AiringSchedule { get; set; }
+}
+
+public record Title
+{
+    [JsonPropertyName("romaji")] 
+    public string Romaji { get; set; } = null!;
+
+    [JsonPropertyName("english")] 
+    public string English { get; set; } = null!;
+}
+
+public record CoverImage
+{
+    [JsonPropertyName("large")] 
+    public string Large { get; set; } = null!;
+}
+
+public record ExternalLink
+{
+    [JsonPropertyName("site")] 
+    public string Site { get; set; } = null!;
+
+    [JsonPropertyName("url")] 
+    public string Url { get; set; } = null!;
+}
+
+public record AiringSchedule
+{
+    [JsonPropertyName("nodes")] 
+    public IEnumerable<Node> Nodes { get; set; }
+}
+
+public record Node
+{
+    [JsonPropertyName("airingAt")] 
+    public int? AiringAt { get; set; }
+
+    [JsonPropertyName("episode")] 
+    public int? Episode { get; set; }
+}
--- AnimeScheduleAPI/Services/AniListService.cs
+++ AnimeScheduleAPI/Services/AniListService.cs
@@ -1,6 +1,61 @@
-﻿namespace AnimeScheduleAPI.Services;
+﻿using System.Text;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using AnimeScheduleAPI.Models;
 
-public class AniListService
+namespace AnimeScheduleAPI.Services;
+
+public class AniListService : IAniListService
 {
-    
+    private readonly HttpClient _httpClient = new HttpClient { BaseAddress = new Uri("https://graphql.anilist.co") };
+
+    public async Task<IEnumerable<Anime>> GetAnimesSchedule()
+    {
+        var query = """
+            query {
+                  Page {
+                    media(season: WINTER, seasonYear: 2024) {
+                      id
+                      title {
+                        romaji
+                        english
+                      }
+                      coverImage {
+                        large
+                      }
+                      episodes
+                      siteUrl
+                      externalLinks {
+                        site
+                        url
+                      }
+                      airingSchedule{
+                        nodes {
+                          id
+                          episode
+                          airingAt
+                        }
+                      }
+                    }
+                  }
+                }
+            """;
+
+        var requestBody = new { query };
+        var jsonString = JsonSerializer.Serialize(requestBody);
+        var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+        var response = await _httpClient.PostAsync("", content);
+        response.EnsureSuccessStatusCode();
+
+        var result = await response.Content.ReadAsStringAsync();
+        var jsonResult = JsonSerializer.Deserialize<ApiResponse>(result);
+
+        return jsonResult.Data.Page.Media;
+    }
+}
+
+public interface IAniListService
+{
+    public Task<IEnumerable<Anime>> GetAnimesSchedule();
 }
